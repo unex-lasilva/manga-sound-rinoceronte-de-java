0cdddbbfb25a819a1d9ff4615a9bf7950a64f80b
Dylan Coêlho Soares <dylancoelhosoares@gmail.com>
2025-04-24 14:04:01 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,56 @@
+public class ReprodutorLista {
+    private ListaReproducao listaReproducao;
+    private String status;
+    private Clip clip;
+
+    public ReprodutorLista(ListaReproducao listaReproducao) {
+        this.listaReproducao = listaReproducao;
+        this.status = "parado";
+        this.clip = null;
+    }
+
+    public void play() {
+
+        System.out.println("Tocando música...");
+        status = "tocando";
+    }
+
+    public void pause() {
+
+        System.out.println("Música pausada.");
+        status = "pausado";
+    }
+
+    public void restartLista() {
+
+        System.out.println("Lista reiniciada.");
+        status = "reiniciado";
+    }
+
+    public void stop() {
+
+        System.out.println();
+        status = "parado" ;
+    }
+
+
+    public void tocarProxima() {
+        if (listaReproducao != null && musicaAtual < listaReproducao.tamanho() - 1) {
+            musicaAtual++;
+            play();
+        } else {
+            System.out.println("Fim da lista.");
+            stop();
+        }
+    }
+
+
+    public void voltarMusica() {
+        if (listaReproducao != null && musicaAtual > 0) {
+            musicaAtual--;
+            play();
+        } else {
+            System.out.println("Início da lista.");
+        }
+    }
+}
